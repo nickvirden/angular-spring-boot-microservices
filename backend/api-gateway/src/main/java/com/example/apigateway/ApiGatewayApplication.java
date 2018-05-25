1393009f8321670cc0496120fb7b11c0f730d1ac
Nick Virden <nickvirden@gmail.com>
2018-05-25 16:10:06 -0700

    Add CORS Filter to app

--- backend/api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ backend/api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -1,8 +1,17 @@
 package com.example.apigateway;
 
+import com.netflix.ribbon.proxy.annotation.Http;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableZuulProxy
@@ -10,6 +19,20 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		config.setAllowedMethods(Arrays.stream(Http.HttpMethod.values()).map(Http.HttpMethod::name).collect(Collectors.toList()));
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+
 	}
 }
 
--- backend/flyway/sql/V2__Create_lists_table.sql
+++ backend/flyway/sql/V2__Create_lists_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE Boards (
     ID serial,
-    USER_ID int NOT NULL REFERENCES Users(ID),
+    # ON DELETE CASCADE deletes orphan data
+    USER_ID int NOT NULL REFERENCES Users(ID) ON DELETE CASCADE,
     LIST_NAME varchar NOT NULL
 );
